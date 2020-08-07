0f6810647562da7b2f2b215d50d923c89171c3f0
nikolatriki <kjofte@yahoo.com>
2020-08-08 01:17:02 +0200

    Create Recipes controller with actions and routes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,38 @@
+class RecipesController < ApplicationController
+  before action :find_recipe, except: %I[index new create]
+
+  def index
+    @recipe = Recipe.all.order(created_at: desc)
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+end
