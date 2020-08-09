6d12631c31aada42d5dd43a35851f834861ec64d
nikolatriki <kjofte@yahoo.com>
2020-08-09 19:44:25 +0200

    Create Recipe model validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ApplicationRecord
+  MINIMUM_TITLE_LENGTH = 4
+  validates :title, presence: true, length: { minimum: MINIMUM_TITLE_LENGTH }
+  validates :description, presence: true
 end
