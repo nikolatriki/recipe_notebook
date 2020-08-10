2264d5221ec694c1f53336c977e3b2b52d2c3490
nikolatriki <kjofte@yahoo.com>
2020-08-10 03:35:42 +0200

    Additional validation test for model Recipe

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :recipe do
     title { 'Recipe title'}
+    description { 'Recipe descrition'}
 
   end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Recipe do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_length_of(:title).is_at_least(Recipe::MINIMUM_TITLE_LENGTH) }
+    it { is_expected.to validate_presence_of(:description) }
   end
 end
