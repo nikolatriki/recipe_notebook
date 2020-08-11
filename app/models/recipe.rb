732a7cd69a954702b3f9975d45543c3de8b96c6a
nikolatriki <kjofte@yahoo.com>
2020-08-11 13:57:51 +0200

    Create associating Ingredient and Instruction models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ApplicationRecord
   MINIMUM_TITLE_LENGTH = 4
+
+  has_many :ingredients
+  has_many :instructions
   validates :title, presence: true, length: { minimum: MINIMUM_TITLE_LENGTH }
   validates :description, presence: true
 end
---
+++ spec/factories/instructions.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :instruction do
+    step { "MyText" }
+  end
+end
