17d9249a102d3bc4255b627abd43751d51a2de19
nikolatriki <kjofte@yahoo.com>
2020-08-13 23:46:27 +0200

    Defined Sessions actions and SessionsHelper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
+    session_notice(:warning, 'You are already logged in!') if logged_in?
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user &.authenticate(params[:session][:password])
+      log_in(user)
+      flash[:success] = "Welcome #{user.handle} !"
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email and/or password'
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
   end
 end
