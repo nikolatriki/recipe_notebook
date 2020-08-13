a76cf52d2dcdadbd1aa1d3dec39c7e4232377a23
nikolatriki <kjofte@yahoo.com>
2020-08-13 16:13:42 +0200

    Create Sessions controler and modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'recipes#index'
   resources :recipes do
     resources :ingredients
@@ -6,5 +7,8 @@ Rails.application.routes.draw do
   end
   get 'users/signup', to: 'users#new'
   resources :users, except: :new
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
