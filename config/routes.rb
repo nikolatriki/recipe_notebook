e28fdbe8f390d54d749cb97747a6f667ec88b4cf
nikolatriki <kjofte@yahoo.com>
2020-08-13 13:26:46 +0200

    Create Users Controller and config.routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     resources :ingredients
     resources :instructions
   end
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
