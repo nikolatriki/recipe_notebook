9dce5b8bce17a0188087e844017b9f0e24910bb9
nikolatriki <kjofte@yahoo.com>
2020-08-16 20:00:21 +0200

    Create before action require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:danger] = 'You must log in first.'
+      redirect_to login_path
+    end
+  end
 end
